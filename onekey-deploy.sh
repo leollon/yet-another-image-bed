d479b3ab9f6eabfceacc1b00b42e7bd9f893571a
leollon <ccsapp.tux@gmail.com>
2019-12-05 23:23:18 +0800

    add:server:use uwsgi when under development.
        auto-reload when python files is changed like flask's server.

--- onekey-deploy.sh
+++ onekey-deploy.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+set -e
 
-if [ $UID != 0 ]
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
+if [ ${UID} != 0 ]
 then
     echo "run this script as root."
     exit 1
 fi
 
-OS_ID=$(grep -i "^id=.*" /etc/os-release | cut -d '=' -f 2)
+OS_ID=$(grep -i "^id=.*" /etc/os-release | cut -d '=' -f2)
+if [ "${OS_ID}" != "ubuntu" ] || [ "${OS_ID}" != "debian"]
+then
+    echo -e "${RED}Only run this script on debian-like OS.${NOCOLOR}"
+    exit 1
+fi
+
 apt-get update && apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -16,19 +26,17 @@ apt-get update && apt-get install \
 curl -fsSL "https://download.docker.com/linux/${OS_ID}/gpg" | apt-key add -
 
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/$OS_ID \
+   "deb [arch=amd64] https://download.docker.com/linux/${OS_ID} \
    $(lsb_release -cs) \
    stable" -y
 
-apt update && apt install docker-ce -yq
+apt-get update && apt-get install -qq docker-ce
 
-curl -L "https://github.com/docker/compose/releases/download/1.22.0/\
+curl -fsSL "https://github.com/docker/compose/releases/download/1.25.0/\
 docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose ./up ./down
 
 ./up --build -d
-
-docker container exec -i "$(docker-compose -f docker-compose.yml ps | grep 'mongo_1' | awk '{print $1}')" mongo < compose/production/mongodb/createUser.js
 
 docker-compose -f docker-compose.yml ps
