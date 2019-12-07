3a922027d9f3fb11195ef6e306667c458e92a24b
leollon <ccsapp.tux@gmail.com>
2019-12-07 23:30:39 +0800

    chord:templates:remove break line.
    chord:shell:add double quotes for variable in shell script

--- compose/production/mongodb/Init_db.sh
+++ compose/production/mongodb/Init_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mongo -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} localhost:27017/admin << EOJS
+mongo -u "${MONGO_INITDB_ROOT_USERNAME}" -p "${MONGO_INITDB_ROOT_PASSWORD}" localhost:27017/admin << EOJS
 use ${MONGO_INITDB_DATABASE};
 db.createUser({user: "${MONGO_USER}", pwd: "${MONGO_USER_PASSWORD}", roles: [{role: "readWrite", db: "${MONGO_INITDB_DATABASE}"}]});
 EOJS
